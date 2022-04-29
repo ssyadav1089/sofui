4c62d7bfc239c976314ea7ff4b5999e97cbef6f2
ssyadav1089 <48791697+ssyadav1089@users.noreply.github.com>
2022-04-29 14:07:04 +0530

    test-commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    );
+  }
 }
